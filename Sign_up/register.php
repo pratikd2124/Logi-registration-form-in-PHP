ccf626f8415f38b23f2ef893f3a54b5e76e7f716
Pratik Dhumal <68268541+pratikd2124@users.noreply.github.com>
2021-03-10 20:35:04 +0530

    Update register.php

--- Sign_up/register.php
+++ Sign_up/register.php
@@ -1,3 +1,81 @@
+<?php 
+require_once "config.php";
+
+$username = $password = $confirmpassword = "";
+$usernam_err =$password_err = $confirmpassword_err ="";
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    // check for empty field username
+    if(empty(trim($_POST["username"]))){
+        $usernam_err = "Username cannot be blank";
+    }
+    else{
+        $sql = "SELECT id FROM users WHERE username = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        if($stmt){
+            mysqli_stmt_bind_param($stmt,"s", $param_username);
+
+            //Set value of param username
+            $param_username = trim($_POST['username']);
+
+            //Try to execute it
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $usernam_err = "This username is taken";
+                }
+                else{
+                    $username = trim($_POST['username']);
+                }
+            }
+            else{
+                echo "something went wrong";
+            }
+        }
+    }
+    mysqli_stmt_close($stmt);
+
+//check password
+if(empty(trim($_POST['password']))){
+    $password_err = "Password cannot be blank";
+}
+elseif(strlen(trim($_POST['password'])) < 5){
+    $password_err ="less than 5 char! Weak";
+}
+else{
+    $password = trim($_POST['password']);
+}
+
+//check for confirm pass
+if(trim($_POST['password']) !== trim($_POST['confirm_password'])){
+    $confirmpassword_err = "Password should match";
+}
+
+//if no err, insert to databse
+if(empty($usernam_err) && empty($password_err) && empty($confirmpassword_err)){
+    $sql = "INSERT INTO users (username, password) VALUES (?,?)" ;
+    $stmt =mysqli_prepare($conn,$sql);
+    if ($stmt){
+        mysqli_stmt_bind_param($stmt,"ss",$param_username,$param_password);
+
+        //set these parameters
+        $param_username = $username;
+        $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+        //try to exec query 
+        if (mysqli_stmt_execute($stmt)){
+            header("location : login.php");
+        }
+        else{
+            echo "Something went wrong!";
+        }
+        mysqli_stmt_close($stmt);
+    }
+    mysqli_close($conn);
+
+}
+}
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -11,28 +89,81 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <form>
-    <div class="form-group">
-    <label for="exampleInputEmail1">Name</label>
-    <input type="name" class="form-control" id="exampleInputname" aria-describedby="emailHelp" placeholder="Enter email">
-  </div> 
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
 
+  <!--nav--> 
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+  </div>
+</nav>  
+  <!-- form -->
+  <div class="container mx-5 mt-4">
+  <h3>Register here</h3><hr>
+  <form action="" method="POST">
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Username</label>
+      <input type="text" class="form-control" name="username" id="inputEmail4" placeholder="Username">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Password</label>
+      <input type="password" class="form-control" name="password" id="inputPassword4" placeholder="Password">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Confirm Password</label>
+      <input type="password" class="form-control" name="confirm_password" id="inputPassword4" placeholder="Confirm Password">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Address 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">City</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputState">State</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Zip</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
+</div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
@@ -43,12 +174,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
     -->
-    <style>
-    form{
-      display: block;
-      margin: 10%;
-    }
-    </style>
+    
   </body>
 </html>
 
